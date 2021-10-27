9e491ffd910b3152e604ea802c8007587286dbbb
Your Name <sauloarqueria@gmail.com>
2021-10-26 21:06:43 -0300

    variáveis do python no HTML páginas de usuarios

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import  Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda', 'Saulo']
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -9,6 +11,10 @@ def home():
 @app.route('/pagina_contatos')
 def contatos():
     return render_template('contatos.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios= lista_usuarios)
 
 if __name__ == '__main__':
     app.run(debug=True)
